b5f8b135233c3bd23d4987ee1a41685464ae06cd
qiqi-xingyi <yuqizhang247@gmail.com>
2025-03-06 00:37:13 -0500

    First Commit

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ def main(instance, token):
     # ====================
     # 0) User-modifiable parameters
     # ====================
-    txt_file_path = "Data/12_13.txt"       # The TXT file containing fragments to be predicted
+    txt_file_path = "Data/8_9.txt"       # The TXT file containing fragments to be predicted
     config_path = "config.txt"           # IBM Quantum config file
     max_fragments = 25                   # Maximum number of fragments
     max_iter = 200                       # Maximum VQE iterations
